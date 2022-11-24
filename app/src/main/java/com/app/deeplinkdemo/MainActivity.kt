195375f052af7a24647e3f1c59deb816768998a1
linh.bui <linh.bui@ntq-solution.com.vn>
2022-11-24 17:25:24 +0700

    change

--- app/src/main/java/com/app/deeplinkdemo/MainActivity.kt
+++ app/src/main/java/com/app/deeplinkdemo/MainActivity.kt
@@ -3,14 +3,20 @@ package com.app.deeplinkdemo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("LinhBD", "MainActivity onCreate")
 
+        if (isTaskRoot) {
+            startActivity(Intent(this, SplashActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            })
+        }
 
-        startActivity(Intent(this, MapActivity::class.java))
-        finish()
+        finishAndRemoveTask()
     }
 }
